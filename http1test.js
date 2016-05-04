6d9914d6bd691ee565c13356b262efd0fb9b03f2
Jason <zhanyouwei@meitunmama.com>
2016-05-04 17:41:53 +0800

    add http2

---
+++ http1test.js
@@ -0,0 +1,25 @@
+/**
+ * @author: Jason.占友伟 zhanyouwei@meitunmama.com
+ * Created on 16/3/9.
+ */
+
+var http = require('http');
+var port = '8001';
+
+http.createServer(function (request, response) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  response.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  response.setHeader("X-Powered-By", ' 3.2.1');
+  var url = request.url;
+  if (url !== '/favicon.ico') {
+    if (url === '/') {
+      response.end('Hello! This is HTTP1.x Server!');
+    } else if (url === '/http1test') {
+      setTimeout(function () {
+        response.end('http1.x success');
+      }, 300);
+    }
+  }
+}).listen(port);
+
---
+++ http2test.js
@@ -0,0 +1,27 @@
+/**
+ * @author: Jason.占友伟 zhanyouwei@meitunmama.com
+ * Created on 16/3/9.
+ */
+var http2 = require('http2');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync(__dirname + '/server.key'),
+  cert: fs.readFileSync(__dirname + '/server.crt'),
+  NPNProtocols: ['h2', 'http 1.1', 'http 1.0']
+};
+
+http2.createServer(options, function (request, response) {
+  var url = request.url;
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  response.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  response.setHeader("X-Powered-By", ' 3.2.1');
+  if (url === '/') {
+    response.end('Hello! This is HTTP2 Server!');
+  } else if (url === '/http2test') {
+    setTimeout(function () {
+      response.end('http2 success');
+    },300);
+  }
+}).listen(8000);
